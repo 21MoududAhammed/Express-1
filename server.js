d4f9ab52e242ba31ad7725111245ba2f97d983d2
21MoududAhammed <21moudud@gmail.com>
2024-05-26 09:25:15 +0600

    Made a static folder instead of declare many routes to send some html files

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
-});
+// static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(8000, () => {
   console.log(`Server in on port 8000.`);
