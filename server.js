435a1baa13bb6fc5d07db79df4983f927dd0cad3
21MoududAhammed <21moudud@gmail.com>
2024-05-26 09:18:17 +0600

    send the index.html and about.html files as response

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require('path');
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ message: "Do what you can, with what you have, where you are." });
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
 app.listen(8000, () => {
